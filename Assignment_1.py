b323236036af1e15cddf0657228659048aa35d91
Ranimali3597 <59640044+Ranimali3597@users.noreply.github.com>
2025-07-08 15:51:05 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Perform operations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+
+# Take user's first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate first name and last name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting message
+print("Hello, " + full_name + "! Welcome to the Python Program")
